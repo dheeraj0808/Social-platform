49e88ab681461b620782cc1ec916bbfd9a6d06b5
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-25 13:04:58 +0530

    backend database setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const app = require("./src/app");
+const db = require("./src/db/db");
 
 const PORT = process.env.PORT || 3000;
+
+db();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
